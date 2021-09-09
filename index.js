52882aa5e0e374921ce86b07480e770f493f5d65
Peter Borrow <borrow.peter83@gmail.com>
2021-09-09 16:01:54 +0100

    REFACTOR: Replaced landing page navigation with dropdown menu

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    document.getElementById('dropdown').classList.toggle('show')
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+    
+});
+
 let slideIndex = 0;
 showSlides();
-
-
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName('image-slides');
@@ -13,3 +25,5 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000);
     }
+
+
